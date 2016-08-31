771b9b684f1f4a608924f40102ae48d14739ab44
Nic McPhee <mcphee@morris.umn.edu>
2016-08-30 21:27:04 -0500

    Remove spurious state test.
    I'd left in a little demo test in `midje_state.clj`; this removes that.

--- test/n_puzzle_as_search/midje_state.clj
+++ test/n_puzzle_as_search/midje_state.clj
@@ -2,9 +2,6 @@
   (:use [midje.sweet])
   (:require [n-puzzle-as-search.state :refer :all]))
 
-(fact (+ 2 2) => 4)
-(fact (+ 2 2) => even?)
-
 (facts
   (:board (->State [[0 1 2] [3 4 5] [6 7 8]] [0 0]))
       => [[0 1 2] [3 4 5] [6 7 8]]

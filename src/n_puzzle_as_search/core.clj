58b8b60b2d8ee177d1af8f00105d94d15b124e23
Nic McPhee <mcphee@morris.umn.edu>
2016-08-30 22:27:58 -0500

    Implement BFS without paths or targets.
    This implements the first, simplest version of bread-first-search based on the Python code in [Introduction to A*](http://www.redblobgames.com/pathfinding/a-star/introduction.html). This doesn't actually check to see if you've found the target state, and it doesn't keep track of how you get from one state to another, but it does manage to search the _entire_ 8-puzzle space in just over 5 seconds on my iMac. Yowza!

--- src/n_puzzle_as_search/core.clj
+++ src/n_puzzle_as_search/core.clj
@@ -1,7 +1,31 @@
 (ns n-puzzle-as-search.core
+  (:require clojure.set
+            [n-puzzle-as-search.state :as s])
   (:gen-class))
+
+(defn breadth-first-search [max-states start-state goal-state]
+  (loop [max-states max-states
+         frontier (conj (clojure.lang.PersistentQueue/EMPTY) start-state)
+         visited #{}]
+    (if (or (neg? max-states)
+            (empty? frontier))
+      visited
+      (when-not (empty? frontier)
+        (let [current (peek frontier)
+              children (set (s/children current))
+              unvisited-children (clojure.set/difference children visited)
+              new-frontier (reduce conj (pop frontier) unvisited-children)
+              new-visited (clojure.set/union children visited)]
+          (recur (- max-states (count unvisited-children))
+                 new-frontier new-visited))))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [start-state (s/->State [[0 1 3] [4 2 5] [7 8 6]] [0 0])
+        goal-state (s/->State [[0 1 2] [3 4 5] [6 7 8]] [0 0])
+        max-states 1000000
+        result (breadth-first-search max-states start-state goal-state)]
+    (println (count result))
+    ; (println result)
+    ))

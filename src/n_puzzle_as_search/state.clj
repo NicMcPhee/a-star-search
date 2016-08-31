e209a6e791ba4d3a38d90f08e2c653a9ff522b61
Nic McPhee <mcphee@morris.umn.edu>
2016-08-30 21:24:38 -0500

    Implemented basic n-puzzle state.
    This is somewhat hard-wired to the 8-puzzle, but it wouldn't be too difficult to generalize it to larger puzzle sizes.

--- src/n_puzzle_as_search/state.clj
+++ src/n_puzzle_as_search/state.clj
@@ -2,17 +2,23 @@
 
 (defrecord State [board blank-position])
 
-(defn swap [board old-x old-y new-x new-y]
-  (
+; 0≤n<3
+(defn legal? [n]
+  (and (<= 0 n) (< n 3)))
+
+(defn swap [board old-pos new-pos]
+  (let [old-value (get-in board old-pos)
+        new-value (get-in board new-pos)]
+    (assoc-in (assoc-in board old-pos new-value)
+              new-pos old-value)))
 
 (defn children [state]
   (let [x (first (:blank-position state))
         y (second (:blank-position state))]
-  (for [dx [-1 1]
-        dy [-1 1]
+  (for [[dx dy] [[-1, 0] [1, 0], [0, -1], [0, 1]]
         :let [new-x (+ x dx)
               new-y (+ y dy)]
         :when (legal? new-x)
         :when (legal? new-y)]
-    (->State (swap (:board state) x y new-x new-y)
+    (->State (swap (:board state) [x y] [new-x new-y])
              [new-x new-y]))))

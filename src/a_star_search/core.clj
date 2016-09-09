5c1b7a34d70fe3dae77f1d5c58d94488a60a7b7e
Nic McPhee <mcphee@morris.umn.edu>
2016-09-09 08:56:51 -0500

    Move search algorithms into new namespace.
    I wanted to separate the search algorithms from the `main` function, so I moved them into their own namespace and added the `children-fn` argument so we can pass in how we want to make children. I also moved `core` into the new `a-star-search` namespace so that `lein run` works.

---
+++ src/a_star_search/core.clj
@@ -0,0 +1,35 @@
+(ns a-star-search.core
+  (:require clojure.set
+            [search.algorithms :as alg]
+            [problems.n-puzzle :as np]
+            [clojure.data.priority-map :as pm])
+  (:gen-class))
+
+(defn prefer-horizontal-cost [s t]
+  (let [s-x (first (:blank-position s))
+        t-x (first (:blank-position t))]
+    (inc (Math/abs (- s-x t-x)))))
+
+(defn -main
+  "Search for a hard-coded N-puzzle target state."
+  [& args]
+  (let [start-state (np/->State [[0 1 3] [4 2 5] [7 8 6]] [0 0])
+        goal-state (np/->State [[0 1 2] [3 4 5] [6 7 8]] [0 0])
+        max-states 1000000
+        costs nil
+        result (alg/breadth-first-search np/children max-states start-state goal-state)
+        ; [result costs] (alg/shortest-path np/children max-states start-state goal-state (constantly 1))
+        ; [result costs] (alg/shortest-path np/children max-states start-state goal-state prefer-horizontal-cost)
+        path (alg/extract-path result start-state goal-state)
+        ]
+    (println (str "We explored " (count result) " states."))
+    (println "The path to the solution is:")
+    (doseq [b (map :board path)]
+      (println b))
+    (when costs
+      (println "The path has" (count path) "steps and its cost is" (costs goal-state)))
+    ))
+
+; (time (-main))
+; or
+; lein run
--- src/search/algorithms.clj
+++ src/search/algorithms.clj
@@ -1,10 +1,9 @@
-(ns search.core
+(ns search.algorithms
   (:require clojure.set
-            [problems.n-puzzle :as np]
             [clojure.data.priority-map :as pm])
   (:gen-class))
 
-(defn breadth-first-search [max-states start-state goal-state]
+(defn breadth-first-search [children-fn max-states start-state goal-state]
   (loop [max-states max-states
          frontier (conj (clojure.lang.PersistentQueue/EMPTY) start-state)
          visited #{}
@@ -14,7 +13,7 @@
             (= (peek frontier) goal-state))
       came-from
       (let [current (peek frontier)
-            children (set (np/children current))
+            children (set (children-fn current))
             unvisited-children (clojure.set/difference children visited)
             new-frontier (reduce conj (pop frontier) unvisited-children)
             new-visited (clojure.set/union children visited)
@@ -24,7 +23,7 @@
                new-visited
                new-came-from)))))
 
-(defn shortest-path [max-states start-state goal-state cost-fn]
+(defn shortest-path [children-fn max-states start-state goal-state cost-fn]
   (loop [max-states max-states
          frontier (pm/priority-map start-state 0)
          came-from {}
@@ -35,7 +34,7 @@
       [came-from cost-so-far]
       (let [current (first (peek frontier))
             current-cost (cost-so-far current)
-            children (set (np/children current))
+            children (set (children-fn current))
             children-costs (reduce #(assoc %1 %2 (+ current-cost (cost-fn current %2))) {} children)
             children-to-add (filter #(or (not (contains? cost-so-far %))
                                          (< (children-costs %) (cost-so-far %))) children)
@@ -55,30 +54,3 @@
       (= current-state start-state) (reverse (conj path start-state))
       :else (recur (came-from current-state)
                    (conj path current-state)))))
-
-(defn prefer-horizontal-cost [s t]
-  (let [s-x (first (:blank-position s))
-        t-x (first (:blank-position t))]
-    (inc (Math/abs (- s-x t-x)))))
-
-(defn -main
-  "Search for a hard-coded target state."
-  [& args]
-  (let [start-state (np/->State [[0 1 3] [4 2 5] [7 8 6]] [0 0])
-        goal-state (np/->State [[0 1 2] [3 4 5] [6 7 8]] [0 0])
-        max-states 1000000
-        costs nil
-        ; result (breadth-first-search max-states start-state goal-state)
-        [result costs] (shortest-path max-states start-state goal-state (constantly 1))
-        ; [result costs] (shortest-path max-states start-state goal-state prefer-horizontal-cost)
-        path (extract-path result start-state goal-state)
-        ]
-    (println (str "We explored " (count result) " states."))
-    (println "The path to the solution is:")
-    (doseq [b (map :board path)]
-      (println b))
-    (when costs
-      (println "The path has" (count path) "steps and its cost is" (costs goal-state)))
-    ))
-
-; (time (-main))

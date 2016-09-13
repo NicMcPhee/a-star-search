1a1f34e8b680fdcff5d55b9e7013370760e71000
Shinny Hu <huxx0229@serenity.morris.umn.edu>
2016-09-13 17:05:22 -0500

    Add an 'add' function to update board

--- src/problems/sudoku.clj
+++ src/problems/sudoku.clj
@@ -1,6 +1,6 @@
 (ns problems.sudoku)
 
-(defrecord State [board filled-spaces])
+(defrecord State [board curr-position])
 
 (defn legal? [board-size n]
   (and (<= 0 n) (< n board-size)))
@@ -12,3 +12,26 @@
 (def cols [:x1 :x2 :x3 :x4 :x5 :x6 :x7 :x8 :x9])
 
 (def filled-spaces [])
+
+
+
+
+
+(def numBank (range 1 10))
+
+
+(defn add [board position number numBank]
+  (let [num (nth a (- number 1))]
+    (assoc-in board position num)))
+
+(add [[0 1 2] [0 0 0] [0 0 0]
+      [3 4 5] [0 0 0] [0 0 0]
+      [6 7 8] [0 0 0] [0 0 0]
+
+      [0 1 2] [0 0 0] [0 0 0]
+      [3 4 5] [0 0 0] [0 0 0]
+      [6 7 8] [0 0 0] [0 0 0]
+
+      [0 1 2] [0 0 0] [0 0 0]
+      [3 4 5] [0 0 0] [0 0 0]
+      [6 7 8] [0 0 0] [0 0 0]] [0 0] 4 numBank)

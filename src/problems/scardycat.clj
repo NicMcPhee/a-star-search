d9a42f4b4736b8ed11cfa2d1102f816916e48d5c
Travis Mitchell <mitch809@pegasus.morris.umn.edu>
2016-09-12 22:44:53 -0500

    working on defn childen, added a commit for Disk?

--- src/problems/scardycat.clj
+++ src/problems/scardycat.clj
@@ -1,15 +1,24 @@
 (ns hanoi)
 
-(defrecord State [[tower-start] [tower-mid] [tower-finish]])
+(defrecord State [[tower-start] [tower-mid] [tower-finish]]);;rename before Wednesday @ 2200!
 
 (defn legal? [[tower1] [tower2]]
   (and (< (peek [tower1]) (peek[tower2]))))
 
-(defn move [[tower1] [tower2]]
-  (let [xx ()
-        yy ()]
-    (assoc-in (assoc-in space_)
-                        )))
 
-(defn children [state]
-  (let
+
+
+
+
+
+
+
+
+
+
+
+;;(defn move [[tower1] [tower2]]
+;;  (let [old ()
+;;        new ()]
+;;    (assoc-in (assoc-in space_)
+;;                        )))

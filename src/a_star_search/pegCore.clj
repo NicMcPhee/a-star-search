b43a807922f7096219d8722ab0db545d84215544
brow3924 <brow3924@morris.umn.edu>
2016-09-12 21:00:41 -0500

    started defining legal boundaries

---
+++ src/a_star_search/pegCore.clj
@@ -0,0 +1,32 @@
+(ns a-star-search.core
+  (:require [search.algorithms :as alg]
+            [problems.n-puzzle :as np])
+  (:gen-class))
+
+(defn print-results [result path costs goal-state]
+  (println (str "We explored " (count result) " states."))
+  (println "The path to the solution is:")
+  (doseq [b (map :board path)]
+    (println b))
+  (when costs
+    (println "The path has" (count path) "steps and its cost is" (costs goal-state))))
+
+(defn -main
+  "Search for a hard-coded N-puzzle target state."
+  [& args]
+  (let [start-state (np/->State [[0 1 3] [4 2 5] [7 8 6]] [0 0]) ;START-STATE: addition of array= array.legnth-1
+        goal-state (np/->State [[0 1 2] [3 4 5] [6 7 8]] [0 0]) ; END-STATE: addition of array = 1
+        max-states 1000000
+        costs nil
+        came-from (alg/breadth-first-search np/children max-states start-state goal-state)
+        ; [came-from costs] (alg/shortest-path np/children (constantly 1)
+        ;                                      max-states start-state goal-state )
+        ; [came-from costs] (alg/shortest-path np/children np/prefer-horizontal-cost
+        ;                                      max-states start-state goal-state)
+        path (alg/extract-path came-from start-state goal-state)]
+    (print-results came-from path costs goal-state)))
+
+; Run (with timing) with
+; (time (-main))
+; or
+; time lein run
--- src/problems/peg_puzzle.clj
+++ src/problems/peg_puzzle.clj
@@ -1,19 +1,25 @@
-(ns problems.n-puzzle)
+(ns problems.peg-puzzle)
 
-(defrecord State [board blank-position])
+(defrecord State [board zeros])
 
-(defn legal? [board-size n]
-  (and (<= 0 n) (< n board-size)))
+(defn legalBoard? [board-size n]
+  (and (<= 3 n) (< n board-size)))
 
-(defn swap [board old-pos new-pos]
+(defn legalR? [index n]
+  (and (<= 0 index) ()))
+
+(defn sum [board] ;sums up array
+  (reduce + board))
+
+(defn swap [board old-pos new-pos mid-pos]
   (let [old-value (get-in board old-pos)
         new-value (get-in board new-pos)]
-    (assoc-in (assoc-in board old-pos new-value)
+    (assoc-in (assoc-in (assoc-in board old-pos new-value) mid-pos 0)
               new-pos old-value)))
 
 (defn children [state]
-  (let [x (first (:blank-position state))
-        y (second (:blank-position state))
+  (let [x (first (:zeros state))
+        y (second (:zeros state))
         board-size (count (:board state))]
   (for [[dx dy] [[-1, 0] [1, 0], [0, -1], [0, 1]]
         :let [new-x (+ x dx)
@@ -24,6 +30,27 @@
              [new-x new-y]))))
 
 (defn prefer-horizontal-cost [s t]
-  (let [s-x (first (:blank-position s))
-        t-x (first (:blank-position t))]
+  (let [s-x (first (:zeros s))
+        t-x (first (:zeros t))]
     (inc (Math/abs (- s-x t-x)))))
+
+
+(defn beginRowIndex [row] ;returns index at beginning of row
+  (let [x row]
+    (reduce + (take x (iterate (fn [n](- n 1)) (- x 1))))))
+
+(defn beginRowIndexList [n]
+  (map beginRowIndex (range 1 (+ 1 n))))
+
+(defn endRowIndex [row] ;returns index at end of row
+  (let [x row]
+    (reduce + (take (- x 1) (iterate (fn [n](- n 1)) x)))))
+
+(defn endRowIndexList [n]
+  (map endRowIndex (range 1 (+ 1 n))))
+
+(defn rowOfIndex [index n]
+  (filter (endRowIndexList n)))
+
+(endRowIndexList 5)
+(beginRowIndexList 5)

3efc3de6d53bac0ee26ea8cb61cef7c0257d44b1
Travis Mitchell <mitch809@pegasus.morris.umn.edu>
2016-09-12 21:40:17 -0500

    We started the project, it is saved under the name 'scardycat.clj'.

---
+++ src/problems/scardycat.clj
@@ -0,0 +1,15 @@
+(ns hanoi)
+
+(defrecord State [[tower-start] [tower-mid] [tower-finish]])
+
+(defn legal? [tower]
+  (and ))
+
+(defn move [space]
+  (let [xx ()
+        yy ()]
+    (assoc-in (assoc-in space_)
+                        )))
+
+(defn children [state]
+  (let

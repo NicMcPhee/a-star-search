23f2c59c9a9cd59312d5c30283fcd7ff5ebe65c7
smil1201 <mill5978@morris.umn.edu>
2016-09-13 13:24:09 -0500

    fix logic error

--- src/problems/sudoku.clj
+++ src/problems/sudoku.clj
@@ -5,10 +5,10 @@
 (defn legal? [board-size n]
   (and (<= 0 n) (< n board-size)))
 
-(def values [range 1 10])
+(def values (set (range 1 10)))
 
-(def rows [y1 y2 y3 y4 y5 y6 y7 y8 y9])
+(def rows [:y1 :y2 :y3 :y4 :y5 :y6 :y7 :y8 :y9])
 
-(def cols [x1 x2 x3 x4 x5 x6 x7 x8 x9])
+(def cols [:x1 :x2 :x3 :x4 :x5 :x6 :x7 :x8 :x9])
 
 (def filled-spaces [])

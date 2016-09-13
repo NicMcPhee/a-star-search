acc505358db62e0ac2163d54f8da9fe9149d8168
Travis Mitchell <mitch809@pegasus.morris.umn.edu>
2016-09-13 14:41:55 -0500

    repaired the errors in the first two functions, still working with children.

--- src/problems/scardycat.clj
+++ src/problems/scardycat.clj
@@ -1,24 +1,24 @@
 (ns hanoi)
 
-(defrecord State [[tower-start] [tower-mid] [tower-finish]]);;rename before Wednesday @ 2200!
+(defrecord State [tower-start tower-mid tower-finish]);;rename before Wednesday @ 2200!
 
 (defn legal? [[tower1] [tower2]]
   (and (< (peek [tower1]) (peek[tower2]))))
 
 
-(defn swap [[tower-start] [tower-mid] [tower-finish]]
+(defn swap [tower-start tower-mid tower-finish]
   (let [old-value (pop [tower-start])
         new-value (cons [tower-mid])]
     ;;dont know what to put for stru yet.
-    (assoc-in stru [tower-start] (get-in [tower-start]))
+    (assoc-in State [tower-start] (get-in [tower-start]))
     ;; over writing the current stru, and adding the old-value to the current tower, dont know if it works
     ;; getting late, see ya
-    (assoc-in stru [tower-mid] (cons old-value [tower-mid]))))
+    (assoc-in State [tower-mid] (cons old-value [tower-mid]))))
 
 
 (defn children [state]
   ;; Not to sure how to call up the disk command
-  (let [disk (peak [tower-mid])]
+  (let [disk (peek state)]
   (for [[T1 T2 T3] [[tower-start] [tower-mid] [tower-finish]]
         :let [new-T1 [tower-start]
               new-T2 [tower-mid]
@@ -32,19 +32,5 @@
     (-> State (swap (:tower state) [T1 T2 T3] [new-T1 new-T2 new-T3])
               [new-T1 new-T2 new-T3]))))
 
-
-
-
-
-
-
-
-
-
-
-
-;;(defn move [[tower1] [tower2]]
-;;  (let [old ()
-;;        new ()]
-;;    (assoc-in (assoc-in space_)
-;;                        )))
+(peek [])
+(peek tower-mid)

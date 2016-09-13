a833df7589541325bf9f85bb0c4fcf99087fd0e0
xiaox232 <xiaox232@morris.umn.edu>
2016-09-12 20:12:56 -0500

    Updated the chindese_ring_puzzle.clj so that we can return a vector of children/child.

--- src/problems/chinese_ring_puzzle.clj
+++ src/problems/chinese_ring_puzzle.clj
@@ -7,23 +7,33 @@
         leadRing (:leadRing state),
         Rings (:Rings state),
         nextRing (+ leadRing 1),
-        R1 (Rings 0),
-        R2 (Rings 1),
-        R3 (Rings 2),
-        R4 (Rings 3)]
+        R1 (nth Rings 0),
+        R2 (nth Rings 1),
+        R3 (nth Rings 2),
+        R4 (nth Rings 3)]
 
-    (if (= R1 "on")
-        (let [Rings1 ["off" R2 R3 R4], leadRing1 (.indexOf Rings1 "on")]
-              (->State leadRing1 Rings1)),
-        (let [Rings1 ["on" R2 R3 R4], leadRing1 (.indexOf Rings1 "on")]
-              (->State leadRing1 Rings1)))
+
+    (let [children (if (= R1 "on")
+        (let [Rings1 ["off" R2 R3 R4]
+              leadRing1 (.indexOf Rings1 "on")]
+              (->State leadRing1 Rings1))
+        (let [Rings1 ["on" R2 R3 R4]
+              leadRing1 (.indexOf Rings1 "on")]
+              (->State leadRing1 Rings1)))]
 
 
     (if (<= nextRing (- len 1))
-        (if (= (get Rings nextRing) "on"),
+        (if (= (get Rings nextRing) "on")
             (let [Rings2 (assoc Rings nextRing "off"), leadRing2 (.indexOf Rings2 "on")]
-                  (->State leadRing2 Rings2)),
+                  (conj children (->State leadRing2 Rings2)))
             (let [Rings2 (assoc Rings nextRing "on"), leadRing2 (.indexOf Rings2 "on")]
-                  (->State leadRing2 Rings2))))))
+                  (conj children (->State leadRing2 Rings2))))
+            [children]))))
+
+
+; (let [x child1]
+ ;  (if (<= nextRing (- len 1))
+ ;    (conj [x] child2)
+ ;    [x]))
 
 

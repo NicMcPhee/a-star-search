459734925648f1c9afc62d9f12aabb70b9868849
Laverne Schrock <schr1230@morris.umn.edu>
2016-09-12 22:58:25 -0500

    Reorder functions
    my-get, get-adj-coords, and end-point-on-board? all needed to be moved
    in order for the file to be evaluatable.

--- src/problems/triangle.clj
+++ src/problems/triangle.clj
@@ -14,6 +14,40 @@
 (def coords (get-all-coords [[][][][][]])) ;; Shhh, it's fine!
 
 
+(defn my-get [row col board]
+  (nth (nth board row) col))
+
+(defn end-point-on-board?
+  "Takes the index of the last row in the board and a jump pair
+    and returns true iff the end point is on the board."
+  [last-row-index {[row col] :end}]
+  (not  (or
+         ;; check for running off the left end of the row
+         ;; (> 0 (second end))
+         (> 0 col)
+
+         ;; check for running off the right end of the row
+         (> col row)
+
+         ;; check for going down too far
+         (< last-row-index row)
+
+         ;; check for going up too far
+         (> 0 row)
+         )))
+
+(defn get-adj-coords
+  "Takes a row and column and returns a vector of positions that you
+   can jump to paired with the position that you will jump over.
+   This function assumes that your grid is infinite so you will have
+   to filter the points returned."
+  [[row column]]
+  [{:over [row (+ column 1)] :end [row (+ column 2)]}
+   {:over [row (- column 1)] :end [row (- column 2)]}
+   {:over [(+ row 1) column] :end [(+ row 2) column]}
+   {:over [(+ row 1) (+ column 1)] :end [(+ row 2) (+ column 2)]}
+   {:over [(- row 1) column] :end [(- row 2) column]}
+   {:over [(- row 1) (- column 1)] :end [(- row 2) (- column 2)]}])
 
 (defn generate-valid-states [[row col] state]
   (if (= (my-get row col state) 0)
@@ -42,41 +76,6 @@
 
 
 
-
-
-(defn get-adj-coords
-  "Takes a row and column and returns a vector of positions that you
-   can jump to paired with the position that you will jump over.
-   This function assumes that your grid is infinite so you will have
-   to filter the points returned."
-  [[row column]]
-  [{:over [row (+ column 1)] :end [row (+ column 2)]}
-   {:over [row (- column 1)] :end [row (- column 2)]}
-   {:over [(+ row 1) column] :end [(+ row 2) column]}
-   {:over [(+ row 1) (+ column 1)] :end [(+ row 2) (+ column 2)]}
-   {:over [(- row 1) column] :end [(- row 2) column]}
-   {:over [(- row 1) (- column 1)] :end [(- row 2) (- column 2)]}])
-
-(defn end-point-on-board?
-  "Takes the index of the last row in the board and a jump pair
-    and returns true iff the end point is on the board."
-  [last-row-index {[row col] :end}]
-  (not  (or
-         ;; check for running off the left end of the row
-         ;; (> 0 (second end))
-         (> 0 col)
-
-         ;; check for running off the right end of the row
-         (> col row)
-
-         ;; check for going down too far
-         (< last-row-index row)
-
-         ;; check for going up too far
-         (> 0 row)
-         )))
-(defn my-get [row col board]
-  (nth (nth board row) col))
 
 (defn filled?
   "Takes a row, column, and board and returns

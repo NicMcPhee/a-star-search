ba66f3fa9ebdcc6c2d7d026f46fc70c0fb5429da
Travis Mitchell <mitch809@pegasus.morris.umn.edu>
2016-09-12 21:51:42 -0500

    updated the legal? command

--- src/problems/scardycat.clj
+++ src/problems/scardycat.clj
@@ -2,10 +2,10 @@
 
 (defrecord State [[tower-start] [tower-mid] [tower-finish]])
 
-(defn legal? [tower]
-  (and ))
+(defn legal? [[tower1] [tower2]]
+  (and (< (peek [tower1]) (peek[tower2]))))
 
-(defn move [space]
+(defn move [[tower1] [tower2]]
   (let [xx ()
         yy ()]
     (assoc-in (assoc-in space_)

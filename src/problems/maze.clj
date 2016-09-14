2fe3a99d65a06e78e74de6e74ddea910924b62af
kylehakala <hakal053@morris.umn.edu>
2016-09-14 18:09:25 -0500

    Adds unfinished children function

--- src/problems/maze.clj
+++ src/problems/maze.clj
@@ -4,8 +4,18 @@
 (defrecord State [board current-pos])
 
 (defn children [state]
+  (let [x (first (:current-pos state))
+        y (second (:current-pos state))
+        board-x (count (:board state))
+        board-y (count (first (:board state)))]
+  (for [[dx dy] [[-1, 0] [1, 0], [0, -1], [0, 1]]
+        :let [new-x (+ x dx)
+              new-y (+ y dy)]
+        :when (legal? board-x board-y [new-x new-y])]
+    (->State (swap (:board state) [x y] [new-x new-y])
+             [new-x new-y]))))
 
-  )
+(def x "x") ;Pay no attention to the x behind the curtain. This varibale lets us use x without quotes.
 
 (defn swap [board old-pos new-pos]
   (let [old-value (get-in board old-pos)
@@ -13,11 +23,11 @@
     (assoc-in (assoc-in board old-pos new-value)
               new-pos old-value)))
 
-(defn legal? [board-width board-height current-pos new-pos]
-  (and (>= (:x current-pos) 0)
-       (>= (:y current-pos) 0)
-       (< (:x current-pos) board-width)
-       (< (:y current-pos) board-height)))
+(defn legal? [board-width board-height new-pos]
+  (and (>= (:x new-pos) 0)
+       (>= (:y new-pos) 0)
+       (< (:x new-pos) board-width)
+       (< (:y new-pos) board-height)))
 
 ;Bits and pieces for sanity's sake
 
@@ -25,3 +35,10 @@
 (:y (->Coord 1 2))
 
 (:x (:current-pos (->State [[1 2] [1 2]] (->Coord 1 2))))
+
+
+(count (first [[0 0 0 0]
+               [0 x x 0]
+               [0 x x 0]
+               [0 0 0 x]
+               [0 x 0 0]]))

0fbecb492296c62566ccc69ee967c72ec8a31e49
rich1143 <rich1143@morris.umn.edu>
2016-09-14 11:18:09 -0500

    Created checks and legal functions
    Created legal function to check if a state is legal.
    Created check functions to check if a new value has any conflicts on the board.

--- richardsmitchellettesudoku.clj
+++ richardsmitchellettesudoku.clj
@@ -22,3 +22,8 @@
 
 
 (defn getBoxValues [board box] (into [] (concat (nth (getBox board box) 0) (nth (getBox board box) 1) (nth (getBox board box) 2))))
+(defn checkBox [board box new-value] (not= (some #{new-value} (getBoxValues board box)) new-value))
+(defn checkRow [board row col new-value] (if (= (getByRowCol board row col) new-value) false (if (< col 8) (checkRow board row (+ col 1) new-value) true)))
+(defn checkCol [board row col new-value] (if (= (getByRowCol board row col) new-value) false (if (< row 8) (checkCol board (+ row 1) col new-value) true)))
+
+(defn legal? [board row col new-value] (and (checkBox board (+ (* 3 (int (/ row 3))) (int (/ col 3))) new-value) (checkRow board row 0 new-value) (checkCol board 0 col new-value)))

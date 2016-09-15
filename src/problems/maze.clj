43d4238424795a57184545b444537cfa759ec4e6
kylehakala <hakal053@morris.umn.edu>
2016-09-14 19:13:29 -0500

    Refactors children functions for readability for debugging.

--- src/problems/maze.clj
+++ src/problems/maze.clj
@@ -1,44 +1,43 @@
 (ns problems.maze)
 
-(defrecord Coord [x y])
 (defrecord State [board current-pos])
 
 (defn children [state]
   (let [x (first (:current-pos state))
         y (second (:current-pos state))
-        board-x (count (:board state))
-        board-y (count (first (:board state)))]
+        board-y (count (:board state))
+        board-x (count (first (:board state)))]
   (for [[dx dy] [[-1, 0] [1, 0], [0, -1], [0, 1]]
         :let [new-x (+ x dx)
               new-y (+ y dy)]
-        :when (legal? board-x board-y [new-x new-y])]
-    (->State (swap (:board state) [x y] [new-x new-y])
-             [new-x new-y]))))
+        :when (legal? board-x board-y [new-x new-y] (:board state))]
+    (->State (:board state) [new-x new-y]))))
 
-(def x "x") ;Pay no attention to the x behind the curtain. This varibale lets us use x without quotes.
+(def x -10) ;Pay no attention to the x behind the curtain. This varibale lets us use x without quotes.
 
-(defn swap [board old-pos new-pos]
-  (let [old-value (get-in board old-pos)
-        new-value (get-in board new-pos)]
-    (assoc-in (assoc-in board old-pos new-value)
-              new-pos old-value)))
+(defn legal? [board-width board-height new-pos board]
 
-(defn legal? [board-width board-height new-pos]
-  (and (>= (:x new-pos) 0)
-       (>= (:y new-pos) 0)
-       (< (:x new-pos) board-width)
-       (< (:y new-pos) board-height)))
+  (true? (>= (first new-pos) 0))
+ ; (>= (second new-pos) 0)
+ ; (< (first new-pos) board-width)
+ ;(< (second new-pos) board-height)
+ ; (> (get-in board new-pos) 0)
+
+  (and (>= (first new-pos) 0)
+       (>= (second new-pos) 0)
+       (< (first new-pos) board-width)
+       (< (second new-pos) board-height)
+       (> (get-in board new-pos) 0)))
+
+(+ x 1)
 
 ;Bits and pieces for sanity's sake
 
-(:x (->Coord 1 2))
-(:y (->Coord 1 2))
 
-(:x (:current-pos (->State [[1 2] [1 2]] (->Coord 1 2))))
+(def test-board [[0 0 0 0]
+                 [x x x 0]
+                 [0 x x 0]
+                 [0 0 0 x]
+                 [0 x 0 0]])
 
-
-(count (first [[0 0 0 0]
-               [0 x x 0]
-               [0 x x 0]
-               [0 0 0 x]
-               [0 x 0 0]]))
+(children (->State test-board [0 1]))

883a1e16b76cac85e4c62a234c1340fc83f53670
Henry Fellows <fello056@morris.umn.edu>
2016-09-14 19:32:23 -0500

    BLOCKS WORKgit status

--- src/problems/sudoku.clj
+++ src/problems/sudoku.clj
@@ -1,7 +1,44 @@
 (ns problems.sudoku)
 
+;;## validation top level ##
+(defn every-element-in-vec-distinct?
+ "Returns True if every element in every vector is unique or nil.
+  Returns False otherwise."
+  [vecs]
+ (every? identity (map #(apply distinct? %) vecs)))
+
+(def every-vector-valid? (memoize every-element-in-vec-distinct?))
+
+;;## validating 3x3 blocks ##
+(defn hop [adjusted-n k chunk]
+  "Given the first number in the block (n), and the offset (k), returns the 3
+  numbers that make up a column in a sudoku block."
+  (take-nth 9 (drop (+ adjusted-n k) chunk)))
+
+(defn hop-block [chunk n]
+  "Gets the three columns in a 3x3 sudoku block."
+  (let [adjusted-n (* n 3)] ;; the upper left hand number in a sudoku block.
+    (into [] (concat (hop adjusted-n 0 chunk)
+                     (hop adjusted-n 1 chunk)
+                     (hop adjusted-n 2 chunk)))))
+
+(defn get-blocks [chunk]
+  "Operates on a 3x9 chunk of a sudoku state. Returns a list of 3 vectors of
+  9 elements that represents a single block (3x3) in sudoku."
+  (map #(hop-block chunk %) [0 1 2])) ;; the 'numbers' of the three blocks
+
+(defn check-squares [state]
+  "Checks that every 3x3 sudoku block has contains distinct elements or nil."
+  (every-vector-valid? (mapcat get-blocks (partition 27 state))))
+
+;;validate horizontal
+(defn check-horizontal
+ "Check that each row contains distinct elements"
+ [state]
+ (every-vector-valid? (partition 9 state)))
+
 (defn do-children
-  "Given a sudo state, a vector of ints, returns all valid child states as a vector of ints"
+  "Given a sudoku state, a vector of ints, returns all valid child states as a vector of ints"
   [state]
  state)
 
@@ -9,21 +46,6 @@
 
 (defn valid-state? [state]
   state)
-
-(defn do-horizontal-valid? [state]
-  (every? identity (map every-unique? (partition 9 state))))
-
-(defn do-cube-valid? [n state]
-  (let [infini-state (cycle state)
-        cube [(nth (* n))]]
-    (concat (nth))))
-
-(def every-unique? (memoize do-every-unique?))
-(defn do-every-unique?
-  "Returns true if every element in the seq is unique or nil.
-   Returns False otherwise."
-  [a-list]
-  (apply unique? a-list))
 
 (def test-vec
   [nil nil nil nil nil nil nil 4 nil
@@ -37,20 +59,26 @@
                  1 2 3 4 5 6 7 8 9
                  1 2 3 4 5 6 7 8 9])
 
-(def test-vec-4 [1 2 3 4 5 6 7 8 9
-                 4 5 6 4 5 6 7 8 9
-                 7 8 9 4 5 6 7 8 9])
+(def test-chunk [ 0  1  2  3  4  5  6  7  8
+                  9 10 11 12 13 14 15 16 17
+                 18 19 20 21 22 23 24 25 26])
 
-(partition 27 test-vec-3)
+(def test-block [ 0  1  2  3  4  5  6  7  8
+                  9 10 11 12 13 14 15 16 17
+                 18 19 20 21 22 23 24 25 26
+                 27 28 29 30 31 32 33 34 35
+                 36 37 38 39 40 41 42 43 44
+                 45 46 47 48 49 50 51 52 53
+                 54 55 56 57 58 59 60 61 62
+                 63 64 65 66 67 68 69 70 71
+                 72 73 74 75 76 77 78 79 80])
 
-(defn get-blocks [chunk]
-  (map #(hop-block chunk %)))
-
-(defn hop-block [chunk n]
-  (let [adjusted-n (* n 3)]
-    (into [] (concat (hop adjusted-n 0 chunk)
-                     (hop adjusted-n 1 chunk)
-                     (hop adjusted-n 2 chunk)))))
-
-(defn hop [adjusted-n k chunk]
-  (take-nth 9 (drop (+ adjusted-n k) chunk)))
+(def test-bad   [ 0  1  2  3  4  5  6  7  8
+                  0  1  2 12 13 14 15 16 17
+                  0  1  2 21 22 23 24 25 26
+                 27 28 29 30 31 32 33 34 35
+                 36 37 38 39 40 41 42 43 44
+                 45 46 47 48 49 50 51 52 53
+                 54 55 56 57 58 59 60 61 62
+                 63 64 65 66 67 68 69 70 71
+                 72 73 74 75 76 77 78 79 80])

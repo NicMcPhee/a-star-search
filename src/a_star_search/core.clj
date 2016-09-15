2879f0f9ec72286d9bf402baa625268fbc618456
Shinny Hu <huxx0229@serenity.morris.umn.edu>
2016-09-14 20:20:03 -0500

    no error on the namespace thing. But it keeps connecting without returning anything

--- src/a_star_search/core.clj
+++ src/a_star_search/core.clj
@@ -16,7 +16,7 @@
 (defn -main
   "Search for a hard-coded sudoku target state."
   [& args]
-  (let [start-state (s/->State [ [[0 0 0 2 6 0 7 0 1]
+  (let [start-state (s/->State [  [0 0 0 2 6 0 7 0 1]
                                   [6 8 0 0 7 0 0 9 0]
                                   [1 9 0 0 0 4 5 0 0]
 
@@ -26,11 +26,11 @@
 
                                   [0 0 9 3 0 0 0 7 4]
                                   [0 4 0 0 5 0 0 3 6]
-                                  [7 0 3 0 1 8 0 0 0]] [0, 0] ])
+                                  [7 0 3 0 1 8 0 0 0]] [0, 0] )
 
 
 
-        goal-state (s/->State [  [[4 3 5 2 6 9 7 8 1]
+        goal-state (s/->State [   [4 3 5 2 6 9 7 8 1]
                                   [6 8 2 5 7 1 4 9 3]
                                   [1 9 7 8 3 4 5 6 2]
 
@@ -40,16 +40,16 @@
 
                                   [5 1 9 3 2 6 8 7 4]
                                   [2 4 8 9 5 7 1 3 6]
-                                  [7 6 3 4 1 8 2 5 9]] [9, 0] ])
+                                  [7 6 3 4 1 8 2 5 9]] [9, 0] )
 
 
 
         max-states 1000000
         costs nil
         came-from (alg/breadth-first-search s/children max-states start-state goal-state)
-        ; [came-from costs] (alg/shortest-path np/children (constantly 1)
+        ; [came-from costs] (alg/shortest-path s/children (constantly 1)
         ;                                      max-states start-state goal-state )
-        ; [came-from costs] (alg/shortest-path np/children np/prefer-horizontal-cost
+        ; [came-from costs] (alg/shortest-path s/children np/prefer-horizontal-cost
         ;                                      max-states start-state goal-state)
         path (alg/extract-path came-from start-state goal-state)]
     (print-results came-from path costs goal-state)))
@@ -58,6 +58,6 @@
 
 
 ; Run (with timing) with
-(time (-main))
+;(time (-main))
 ; or
 ; time lein run

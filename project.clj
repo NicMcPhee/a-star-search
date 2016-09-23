4ac8b0dbe565b6bd7b5e3e0308b760f6185c6817
thele116 <thele116@morris.umn.edu>
2016-09-23 02:12:21 -0500

    try to correct midje in project.clj

--- project.clj
+++ project.clj
@@ -8,4 +8,5 @@
   :main ^:skip-aot a-star-search.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
-             :dev {:dependencies [[midje "1.8.3"]]}})
+             :dev {:dependencies [[midje "1.8.3" :exclusions [org.clojure/clojure]]]
+	     		:plugins [[lein-midje "3.2.1"]]}})

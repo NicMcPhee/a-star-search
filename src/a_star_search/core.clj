90dc0848ab7ed125db2768dd802ffeb42f2e3b45
Shinny Hu <huxx0229@sentinel.morris.umn.edu>
2016-09-25 14:21:42 -0500

    change the sudoku a bit to easy assess heuristic working

--- src/a_star_search/core.clj
+++ src/a_star_search/core.clj
@@ -16,13 +16,13 @@
 (defn -main
   "Search for a hard-coded sudoku target state."
   [& args]
-  (let [start-state (s/->State [  [8 4 1 9 7 3 2 5 6]
+  (let [start-state (s/->State [  [0 0 1 9 7 3 0 5 6]
                                   [0 9 0 0 8 0 0 0 0]
                                   [0 0 0 0 0 0 3 0 0]
 
                                   [5 0 0 0 0 1 0 0 0]
                                   [0 0 0 0 0 0 0 4 0]
-                                  [4 7 6 3 5 8 1 9 2]
+                                  [4 7 0 3 0 0 1 9 2]
 
                                   [0 0 0 0 4 0 0 0 0]
                                   [3 0 2 0 0 5 6 0 0]

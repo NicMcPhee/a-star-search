3c86f0145c0a91c9c84f231544f138be8ba4dcac
brow3924 <brow3924@morris.umn.edu>
2016-09-27 20:55:14 -0500

    made a-star--basically copied shortest path

--- src/search/algorithmsPeg.clj
+++ src/search/algorithmsPeg.clj
@@ -54,3 +54,31 @@
       (= current-state start-state) (reverse (conj path start-state))
       :else (recur (came-from current-state)
                    (conj path current-state)))))
+
+
+
+
+
+
+(defn a-star [heuristic-fn children-fn cost-fn max-states start-state goal-state]
+  (loop [max-states max-states
+         frontier (pm/priority-map start-state 0)
+         came-from {}
+         cost-so-far {start-state 0}]
+    (if (or (neg? max-states)
+            (empty? frontier)
+            (= (first (peek frontier)) goal-state))
+      [came-from cost-so-far]
+      (let [current (first (peek frontier))
+            current-cost (cost-so-far current)
+            children (set (children-fn current))
+            children-costs (reduce #(assoc %1 %2 (+ current-cost (cost-fn current %2))) {} children)
+            children-to-add (filter #(or (not (contains? cost-so-far %))
+                                         (> (children-costs %) (cost-so-far %))) children)
+            new-cost-so-far (reduce #(assoc %1 %2 (children-costs %2)) cost-so-far children-to-add)
+            new-frontier (reduce #(assoc %1 %2(+ (children-costs %2) (heuristic-fn current))) (pop frontier) children-to-add)
+            new-came-from (reduce #(assoc %1 %2 current) came-from children-to-add)]
+        (recur (- max-states (count children-to-add))
+               new-frontier
+               new-came-from
+               new-cost-so-far)))))

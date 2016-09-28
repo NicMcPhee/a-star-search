de89ef56eb8fee245137587a9d69846649072a90
brow3924 <brow3924@morris.umn.edu>
2016-09-27 20:54:42 -0500

    added a-star function and some notes

--- src/a_star_search/pegCore.clj
+++ src/a_star_search/pegCore.clj
@@ -19,10 +19,14 @@
         max-states 1000000
         costs nil
         ;came-from (alg/breadth-first-search np/children max-states start-state goal-state)
-         [came-from costs] (alg/shortest-path np/children (constantly 1)
-                                              max-states start-state goal-state )
+        ;[came-from costs] (alg/shortest-path np/children (constantly 1)
+        ;                                    max-states start-state goal-state )
+        ;; NOTE: if "constantly 1" is changed to "constantly 0" in shortest-path AND
+        ;; in a-star, both are improved significantly... we don't know why!
         ; [came-from costs] (alg/shortest-path np/children np/prefer-horizontal-cost
         ;                                      max-states start-state goal-state)
+         [came-from costs] (alg/a-star np/heuristic np/children (constantly 1)
+                                              max-states start-state goal-state )
         path (alg/extract-path came-from start-state goal-state)]
     (print-results came-from path costs goal-state)))
 

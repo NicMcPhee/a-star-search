eb01fe1f6652baecd6872ea4be4d718fe66a74f2
Travis Mitchell <mitch809@falcon.morris.umn.edu>
2016-09-28 22:05:50 -0500

    about to switch to devo branch

--- src/a_star_search/core.clj
+++ src/a_star_search/core.clj
@@ -29,8 +29,8 @@
                                           max-states start-state goal-state )
         ;;[came-from costs] (alg/shortest-path np/children np/prefer-horizontal-cost
                                             ;; max-states start-state goal-state)
-         came-from (alg/heuristic-search np/children np/keep-down-piling start-state goal-state :max-states 10000)
-         ;;came-from (alg/heuristic-search-a-star np/children np/keep-down-piling start-state goal-state :max-states 10000)
+         ;;came-from (alg/heuristic-search np/children np/keep-down-piling start-state goal-state :max-states 10000)
+         came-from (alg/heuristic-search-a-star cost-fn np/children np/keep-down-piling start-state goal-state :max-states 10000)
         path (alg/extract-path came-from start-state goal-state)]
     (print-results came-from path costs goal-state)))
 

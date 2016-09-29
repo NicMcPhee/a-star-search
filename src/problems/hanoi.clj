10cca9594438d7c6212d550f3494c13d57eac593
mitch723 <mitch723@morris,umn.edu>
2016-09-28 19:53:46 -0500

    Working on the code for heuristic-search having trouble assinging wight to the problem

--- src/problems/hanoi.clj
+++ src/problems/hanoi.clj
@@ -15,27 +15,32 @@
         new-new (conj (nth towers new-t) disk)]
     (assoc (assoc towers old-t new-old) new-t new-new)))
 
-;; (def towersinside [[5 4 3 2 1] [] []])
-;; (def towers (->State towersinside))
-;; (println towersinside)
-;; (println (nth (:towers towers) 0))
+(def towersinside [[5 4 3 2 1] [] [] [] [] []])
+(def towers (->State towersinside))
+(println towersinside)
+(println (count (nth (:towers towers) 0)))
 
 (defn children [state]
-  (for [[old new] [[0, 1] [1, 0], [0, 2], [1, 2] [2, 1], [2, 0]]
+  (for [[old new] [[0, 1] [1, 0], [0, 2], [1, 2] [2, 1], [2, 0], ;4td tower->
+                   [0, 3], [1, 3], [2, 3], [3, 0], [3,1], [3,2], ;5th tower ->
+                   [0, 4], [1, 4], [2, 4], [3, 4], [4, 0], [4, 1], [4, 2], [4,3]]
         :when (legal? (:towers state)  old new)]
     (->State (swap (:towers state) old new))))
 
 (defn prefer-horizontal-cost [old new]
       (if (empty? (or old new)) 0 1))
 
-;; (defn prefer-horizontal-cost [cur nex]
-;;   (for [[old new] [[0,1] , [0,2] , [1,2] , [1,0] , [2,0] , [2,1]]
-;;         :when (= (peek (nth (:towers cur) old)) (peek (nth (:towers nex) new)))]
-;;     (+ (peek (nth (:towers cur) old)) (Math/abs (- old new)))))
+;; (defn state->vec [state]
+;;   (let [T1 (count (nth (:towers state) 0))
+;;         T2 (count (nth (:towers state) 1))
+;;         T3 (count (nth (:towers state) 2))
+;;         T4 (count (nth (:towers state) 3))
+;;         T5 (count (nth (:towers state) 4))]
+;;     (map / [T1 T2 T3 T4 T5] 15)))
 
 
-;; (defn prefer-horizontal-cost [s t]
-;;   (let [s-x (first (:blank-position s))
-;;         t-x (first (:blank-position t))]
-;;     (inc (Math/abs (- s-x t-x)))))
 
+
+
+;; (defn num-non-blank-wrong [goal-state current-state]
+;;   (count
